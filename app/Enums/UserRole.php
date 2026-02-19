3e55ac7e42bd7f2c268a3bc89e7fe8b4cf123085
Archie Em <archie.em99@gmail.com>
2026-02-19 19:05:57 +0800

    feat(user-enum): add id and label getter

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -2,8 +2,24 @@
 
 namespace App\Enums;
 
-enum UserRole
+enum UserRole: string
 {
     case SystemAdministrator = 'system_administrator';
     case Staff = 'staff';
+
+    public function id(): int
+    {
+        return match ($this) {
+            self::SystemAdministrator => 1,
+            self::Staff => 2,
+        };
+    }
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::SystemAdministrator => 'System Administrator',
+            self::Staff => 'Staff',
+        };
+    }
 }
