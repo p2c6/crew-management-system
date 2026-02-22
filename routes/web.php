a701caebbc1c9357eca1535d8e506ff39f48e2f8
Archie Em <archie.em99@gmail.com>
2026-02-22 19:14:34 +0800

    feat: update route group; add routes for document; update dashboard redirection

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SystemAdministrator\DashboardController as AdminDashboardController;
-use App\Http\Controllers\Staff\DashboardController as StaffDashboardController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\SystemAdministrator\Dashboard\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Staff\Dashboard\DashboardController as StaffDashboardController;
+use App\Http\Controllers\SystemAdministrator\Document\DocumentController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -25,12 +25,18 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::controller(AdminDashboardController::class)
-    ->prefix('/system-administrator')
+    Route::prefix('/system-administrator')
     ->name('system-administrator.')
     ->group(function() {
-        Route::get('/', fn() =>  redirect()->route('system-administrator.dashboard'))->name('index');
-        Route::get('/dashboard', 'index')->name('dashboard');
+        Route::get('/', fn() =>  redirect()->route('system-administrator.dashboard'))->name('system-administrator.root');
+        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+        Route::controller(DocumentController::class)
+        ->prefix('/documents')
+        ->name('documents.')
+        ->group(function() {
+            Route::get('/', 'index')->name('index');
+        });
     });
     
     Route::controller(StaffDashboardController::class)
