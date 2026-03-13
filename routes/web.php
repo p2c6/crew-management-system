a70ac0d7b672a7802b21c0f215274cfd8ef157a1
Archie Em <archie.em99@gmail.com>
2026-03-13 23:19:39 +0800

    feat: add upload document route

--- routes/web.php
+++ routes/web.php
@@ -32,98 +32,101 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::controller(UploadController::class)
-    ->group(function() {
-        Route::post('/upload', 'store');
-        Route::delete('/upload', 'revert');
-    });
+        ->group(function () {
+            Route::post('/upload', 'store');
+            Route::delete('/upload', 'revert');
+        });
 
     Route::prefix('/system-administrator')
-    ->name('system-administrator.')
-    ->group(function() {
-        Route::get('/', fn() =>  redirect()->route('system-administrator.dashboard'))->name('system-administrator.root');
-        Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+        ->name('system-administrator.')
+        ->group(function () {
+            Route::get('/', fn() =>  redirect()->route('system-administrator.dashboard'))->name('system-administrator.root');
+            Route::get('/dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
 
-        Route::controller(DocumentController::class)
-        ->prefix('/documents')
-        ->name('documents.')
-        ->group(function() {
-            Route::get('/', 'index')->name('index');
-            Route::get('/{document}', 'show')->name('show');
-            Route::post('/store', 'store')->name('store');
-            Route::put('/{document}', 'update')->name('update');
-            Route::delete('/{document}', 'destroy')->name('destroy');
+            Route::controller(DocumentController::class)
+                ->prefix('/documents')
+                ->name('documents.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/{document}', 'show')->name('show');
+                    Route::post('/store', 'store')->name('store');
+                    Route::put('/{document}', 'update')->name('update');
+                    Route::delete('/{document}', 'destroy')->name('destroy');
+                });
+
+            Route::controller(DocumentTypeController::class)
+                ->prefix('/document-types')
+                ->name('document-types.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/{documentType}', 'show')->name('show');
+                    Route::post('/store', 'store')->name('store');
+                    Route::put('/{documentType}', 'update')->name('update');
+                    Route::delete('/{documentType}', 'destroy')->name('destroy');
+                });
+
+            Route::controller(RoleController::class)
+                ->prefix('/roles')
+                ->name('roles.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/{role}', 'show')->name('show');
+                    Route::post('/store', 'store')->name('store');
+                    Route::put('/{role}', 'update')->name('update');
+                    Route::delete('/{role}', 'destroy')->name('destroy');
+                });
+
+            Route::controller(RankController::class)
+                ->prefix('/ranks')
+                ->name('ranks.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/{rank}', 'show')->name('show');
+                    Route::post('/store', 'store')->name('store');
+                    Route::put('/{rank}', 'update')->name('update');
+                    Route::delete('/{rank}', 'destroy')->name('destroy');
+                });
+
+            Route::controller(UserController::class)
+                ->prefix('/users')
+                ->name('users.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/{user}', 'show')->name('show');
+                    Route::post('/store', 'store')->name('store');
+                    Route::put('/{user}', 'update')->name('update');
+                    Route::delete('/{user}', 'destroy')->name('destroy');
+                });
+
+            Route::controller(CrewController::class)
+                ->prefix('/crews')
+                ->name('crews.')
+                ->group(function () {
+                    Route::get('/', 'index')->name('index');
+                    Route::get('/create', 'create')->name('create');
+                    Route::get('/{crew}/edit', 'edit')->name('edit');
+                    Route::name('documents.')
+                        ->group(function () {
+                            Route::get('/{crew}/edit/documents', 'documents')->name('index');
+                            Route::post('documents/store', 'storeDocument')->name('store');
+                            Route::get('/documents/{document}/view', 'view')->name('view');
+                            Route::put('/documents/{document}/update', 'updateDocument')->name('update');
+                            Route::delete('/documents/{document}/destory', 'destroyDocument')->name('destroy');
+                        });
+                    Route::get('/{crew}', 'show')->name('show');
+                    Route::post('/store', 'store')->name('store');
+                    Route::put('/{crew}', 'update')->name('update');
+                    Route::delete('/{crew}', 'destroy')->name('destroy');
+                });
         });
 
-        Route::controller(DocumentTypeController::class)
-        ->prefix('/document-types')
-        ->name('document-types.')
-        ->group(function() {
-            Route::get('/', 'index')->name('index');
-            Route::get('/{documentType}', 'show')->name('show');
-            Route::post('/store', 'store')->name('store');
-            Route::put('/{documentType}', 'update')->name('update');
-            Route::delete('/{documentType}', 'destroy')->name('destroy');
-        });
-        
-        Route::controller(RoleController::class)
-        ->prefix('/roles')
-        ->name('roles.')
-        ->group(function() {
-            Route::get('/', 'index')->name('index');
-            Route::get('/{role}', 'show')->name('show');
-            Route::post('/store', 'store')->name('store');
-            Route::put('/{role}', 'update')->name('update');
-            Route::delete('/{role}', 'destroy')->name('destroy');
-        });
-
-        Route::controller(RankController::class)
-        ->prefix('/ranks')
-        ->name('ranks.')
-        ->group(function() {
-            Route::get('/', 'index')->name('index');
-            Route::get('/{rank}', 'show')->name('show');
-            Route::post('/store', 'store')->name('store');
-            Route::put('/{rank}', 'update')->name('update');
-            Route::delete('/{rank}', 'destroy')->name('destroy');
-        });
-
-        Route::controller(UserController::class)
-        ->prefix('/users')
-        ->name('users.')
-        ->group(function() {
-            Route::get('/', 'index')->name('index');
-            Route::get('/{user}', 'show')->name('show');
-            Route::post('/store', 'store')->name('store');
-            Route::put('/{user}', 'update')->name('update');
-            Route::delete('/{user}', 'destroy')->name('destroy');
-        });
-
-        Route::controller(CrewController::class)
-        ->prefix('/crews')
-        ->name('crews.')
-        ->group(function() {
-            Route::get('/', 'index')->name('index');
-            Route::get('/create', 'create')->name('create');
-            Route::get('/{crew}/edit', 'edit')->name('edit');
-            Route::name('documents.')
-            ->group(function() {
-                Route::get('/{crew}/edit/documents', 'documents')->name('index');
-                Route::post('documents/store', 'storeDocument')->name('store');
-            });
-            Route::get('/{crew}', 'show')->name('show');
-            Route::post('/store', 'store')->name('store');
-            Route::put('/{crew}', 'update')->name('update');
-            Route::delete('/{crew}', 'destroy')->name('destroy');
-        });
-    });
-    
     Route::controller(StaffDashboardController::class)
-    ->prefix('/staff')
-    ->name('staff.')
-    ->group(function() {
-        Route::get('/', fn() =>  redirect()->route('staff.dashboard'))->name('index');
-        Route::get('/dashboard', 'index')->name('dashboard');
-    });
+        ->prefix('/staff')
+        ->name('staff.')
+        ->group(function () {
+            Route::get('/', fn() =>  redirect()->route('staff.dashboard'))->name('index');
+            Route::get('/dashboard', 'index')->name('dashboard');
+        });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
