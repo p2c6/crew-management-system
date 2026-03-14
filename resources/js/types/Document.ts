bba300753bce356aa340306791f37050b7b688cd
Archie Em <archie.em99@gmail.com>
2026-03-14 20:03:48 +0800

    feat: add store & update document form types

--- resources/js/types/Document.ts
+++ resources/js/types/Document.ts
@@ -1,4 +1,38 @@
+import { Crew } from "./Crew";
+import { DocumentType } from "./DocumentType";
+
 export type Document = {
     id: number;
-    name: string;
+    crew: Crew;
+    document_type: DocumentType;
+    file_name: string;
+    file_path: string;
+    code: string;
+    issued_date: string;
+    expiry_date: string;
+}
+
+type DocumentRelations = 'crew' | 'document_type' | 'user';
+
+type DocumentFormExcluded =
+  | DocumentRelations
+  | 'id'
+  | 'file_name'
+  | 'file_path';
+
+export type StoreDocumentForm = Omit<Document, DocumentFormExcluded> & {
+    crew_id: number | string;
+    document_type_id: number | string;
+    documents: (File | string | any)[]
+};
+
+export type UpdateDocumentForm = Omit<Document, DocumentFormExcluded> & {
+    document_type_id: number | string;
+    documents: (File | string | any)[]
+    existing_document_id?: number | null;
+    remove_existing_document?: boolean;
+};
+
+export type DeleteDocumentForm = {
+    id: number;
 }
