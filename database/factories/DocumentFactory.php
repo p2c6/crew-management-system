f67a6badc91ff00668ab81960e85c162f2a97bed
Archie Em <archie.em99@gmail.com>
2026-03-15 19:33:02 +0800

    feat: update file name and file path value

--- database/factories/DocumentFactory.php
+++ database/factories/DocumentFactory.php
@@ -20,13 +20,15 @@ class DocumentFactory extends Factory
      */
     public function definition(): array
     {
+        $fileName = fake()->uuid() . '.pdf';
+
         return [
             'crew_id' => Crew::inRandomOrder()->value('id'),
             'document_type_id' => DocumentType::inRandomOrder()->value('id'),
             'user_id' => User::inRandomOrder()->value('id'),
-            'file_name' => fake()->randomLetter(),
-            'file_path' => fake()->randomLetter(),
-            'code' =>  Str::upper(fake()->bothify('??')),
+            'file_name' =>  $fileName,
+            'file_path' => 'documents/' . $fileName,
+            'code' =>  Str::upper(fake()->bothify('???')),
             'issued_date' => fake()->date(),
             'expiry_date' => fake()->date(),
         ];
