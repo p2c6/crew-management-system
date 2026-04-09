ef1dc181613481e788c2ba89d44ec80ef1e80c7c
Archie Em <archie.em99@gmail.com>
2026-04-09 21:48:04 +0800

    feat: display dynamic full name

--- resources/js/Components/NavUser.vue
+++ resources/js/Components/NavUser.vue
@@ -28,7 +28,7 @@ import {
 import { router } from '@inertiajs/vue3'
 
 interface User {
-  name: string
+  full_name: string
   email: string
   avatar: string
 }
@@ -54,13 +54,13 @@ const logout = () => {
             class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
           >
             <Avatar class="h-8 w-8 rounded-lg grayscale">
-              <AvatarImage :src="user.avatar" :alt="user.name" />
+              <AvatarImage :src="user.avatar" :alt="user.full_name" />
               <AvatarFallback class="rounded-lg">
                 CN
               </AvatarFallback>
             </Avatar>
             <div class="grid flex-1 text-left text-sm leading-tight">
-              <span class="truncate font-medium">{{ user.name }}</span>
+              <span class="truncate font-medium">{{ user.full_name }}</span>
               <span class="text-muted-foreground truncate text-xs">
                 {{ user.email }}
               </span>
@@ -77,13 +77,13 @@ const logout = () => {
           <DropdownMenuLabel class="p-0 font-normal">
             <div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
               <Avatar class="h-8 w-8 rounded-lg">
-                <AvatarImage :src="user.avatar" :alt="user.name" />
+                <AvatarImage :src="user.avatar" :alt="user.full_name" />
                 <AvatarFallback class="rounded-lg">
                   CN
                 </AvatarFallback>
               </Avatar>
               <div class="grid flex-1 text-left text-sm leading-tight">
-                <span class="truncate font-medium">{{ user.name }}</span>
+                <span class="truncate font-medium">{{ user.full_name }}</span>
                 <span class="text-muted-foreground truncate text-xs">
                   {{ user.email }}
                 </span>
