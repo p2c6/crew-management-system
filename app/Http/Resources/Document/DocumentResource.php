caf669896395484e13859947309825d8d6ec133b
Archie Em <archie.em99@gmail.com>
2026-04-12 19:25:00 +0800

    feat(expiry-date): implement conditional bg color

--- app/Http/Resources/Document/DocumentResource.php
+++ app/Http/Resources/Document/DocumentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Document;
 
+use App\Http\Resources\DocumentType\DocumentTypeResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,13 +19,21 @@ class DocumentResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'crew_id' => $this->crew_id,
-            'document_type_id' => $this->document_type_id,
+            'document_type' => $this->whenLoaded('documentType', fn() => new DocumentTypeResource($this->documentType)),
             'user_id' => $this->person_in_charge_id,
             'file_name' => $this->file_name,
             'file_path' => $this->file_path,
             'code' => $this->code,
-            'issued_date' => $this->issued_date,
-            'expiry_date' =>  $this->expiry_date,
+            'issued_date' => $this->getDateFormats($this->issued_date),
+            'expiry_date' =>  $this->getDateFormats($this->expiry_date),
+        ];
+    }
+
+    public function getDateFormats(object $date): array
+    {
+        return [
+            'original' => $date,
+            'for_human' => $date->toFormattedDateString(),
         ];
     }
 }
